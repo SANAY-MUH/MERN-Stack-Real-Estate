7aff7ef4f45eab35eb7929d4accb09d1267ff5c9
SANAY-MUH <ssadaalyy@gmail.com>
2026-07-07 10:43:46 +0500

    project created and initialized
    A folder named client is created and tailwindcss is installed and configured successfully

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='text-red-500'>I am Running!</div>
+    </>
+  )
+}
+
+export default App
