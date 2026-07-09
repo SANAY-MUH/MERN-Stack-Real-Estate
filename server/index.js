991ab57f8e8aa1c7e9423526aeb4fb0267fe10cb
SANAY-MUH <ssadaalyy@gmail.com>
2026-07-09 22:20:33 +0500

    Routes creation
    user routes and authentication routes are added

--- server/index.js
+++ server/index.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.route.js' // As we exported it as default, we can change its name but the address should be the same!
+import authRouter from './routes/auth.route.js'
 
 dotenv.config()
 
@@ -9,7 +11,20 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err)=>{
     console.log(err)
 })
+
 const app = express()
+
+app.use(express.json())
+
 app.listen(8000, ()=>{
     console.log("The server is running on port 8000")
 })
+
+//define all routes here
+app.use("/api/user", userRouter)
+app.use("/api/auth", authRouter)
+
+// Test API
+// app.get('/test', (req, res)=> {
+//     res.json({message: "Hello World!"})
+// }); 
