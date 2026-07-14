91a875909d5f59ccb3e24f91c6ca119feb26c5ef
SANAY-MUH <ssadaalyy@gmail.com>
2026-07-14 10:12:59 +0500

    SIgn-in API route
    created signin functionality and route successfully

--- server/controller/auth.controller.js
+++ server/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from '../models/user.model.js'
 import bcrypt from 'bcryptjs'
 import { errorHandler } from '../utilities/error.js'
+import jwt from 'jsonwebtoken'
 
 export const signup = async (req, res, next) => {
     const {username, email, password} = req.body
@@ -12,5 +13,26 @@ export const signup = async (req, res, next) => {
     res.status(201).json('User created Successfully!')
     } catch (error){
         next(errorHandler(500, error.message))
+    }
+}
+
+export const signin = async (req, res, next) => {
+    const {email, password} = req.body
+    try{
+    const validUser = await User.findOne({email})
+    if(!validUser) return next(errorHandler(404, 'User not found!'))
+    const validPassword = bcrypt.compareSync(password, validUser.password)
+    if(!validPassword) return next(errorHandler(401, 'Wrong credientials'))
+    //JWT Token
+    const token = jwt.sign({id: validUser._id}, process.env.JWT_SECRET)
+    //Dont send hashed password to the user
+    const {password: pass, ...rest} = validUser._doc
+    res
+    .cookie('access_token', token, {httpOnly: true})
+    .status(200)
+    .json(rest)
+    }
+    catch(error){
+        next(error)
     }
 }
